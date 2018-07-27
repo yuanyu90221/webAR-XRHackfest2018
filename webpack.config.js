ccc896f820d9cd33802b90e02b98d9dc073fda20
json <json@cbx.io>
2018-07-27 16:22:48 +0800

    upadte

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: `${__dirname}/index.html`,
     filename: 'index.html',
@@ -16,7 +17,7 @@ module.exports = {
     // output 是放入產生出來的結果的相關參數
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'index_bundle.js',
+      filename: '[name]_bundle.js',
     },
     module: {
         // loaders 則是放欲使用的 loaders，在這邊是使用 babel-loader 將所有 .js（這邊用到正則式）相關檔案（排除了 npm 安裝的套件位置 node_modules）轉譯成瀏覽器可以閱讀的 JavaScript。preset 則是使用的 babel 轉譯規則，這邊使用 react、es2015。若是已經單獨使用 .babelrc 作為 presets 設定的話，則可以省略 query
